cce6540a0bedbe456ee5ec8a011b8b2e1ac2f12e
Markus M <markus@mutas.dev>
2021-05-12 20:55:00 +0200

    update

--- JarvisSav/src/app/component/stt/stt.component.spec.ts
+++ JarvisSav/src/app/component/stt/stt.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { STTComponent } from './stt.component';
 
@@ -6,7 +6,7 @@ describe('STTComponent', () => {
   let component: STTComponent;
   let fixture: ComponentFixture<STTComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ STTComponent ]
     })
